d49ef1b148404329c39557641ba9ddb2575254e1
mbh95 <matthewbh95@gmail.com>
2020-03-27 02:25:04 -0700

    Updated one of the examples to use multiple lines.

--- index.tsx
+++ index.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 
 ReactDOM.render(
     <div>
-        <DynamicView initialSrc={"rgb * % x t x  % y t * x y"}/>
+        <DynamicView initialSrc={"rgb\n(* (% x t) x)\n(% y t)\n(* x y)"}/>
         <DynamicView initialSrc={"rgb % t % x y * t * x y / t / x y"}/>
         <DynamicView initialSrc={"rgb (* (* t x) y) (* (- 1 t) (- x y)) (* (- 0.5 t) (- 1 x))"}/>
     </div>, document.getElementById('root'));
