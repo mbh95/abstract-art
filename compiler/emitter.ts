7d5c0aa21b2cd580d878a8d21423a01235e38df1
mbh95 <matthewbh95@gmail.com>
2020-03-28 03:18:26 -0700

    Moved all custom function definitions in glsl header.

--- compiler/emitter.ts
+++ compiler/emitter.ts
@@ -27,10 +27,10 @@ const emitter: Map<TerminalType, (exp: Expression) => string> = Map<TerminalType
     .set(TerminalType.OP_TAN, generateFnEmitter("tan"))
     .set(TerminalType.OP_FLOOR, generateFnEmitter("floor"))
     .set(TerminalType.OP_CEIL, generateFnEmitter("ceil"))
-    .set(TerminalType.OP_ROUND, (exp) => `floor(0.5 + ${emit(exp.args[0])})`)
-    .set(TerminalType.OP_USHIFT, (exp) => `(${emit(exp.args[0])} + 1.0) / 2.0`)
-    .set(TerminalType.OP_RGB, (exp) => `vec3(${exp.args.map(arg => `${emit(arg)}.x`).join(", ")})`)
-    .set(TerminalType.OP_BW, (exp) => `vec3(0.3 * (${emit(exp.args[0])}).x + 0.59 * (${emit(exp.args[0])}).y + 0.11 * (${emit(exp.args[0])}).z)`);
+    .set(TerminalType.OP_ROUND, generateFnEmitter("round"))
+    .set(TerminalType.OP_USHIFT, generateFnEmitter("ushift"))
+    .set(TerminalType.OP_RGB, generateFnEmitter("rgb"))
+    .set(TerminalType.OP_BW, generateFnEmitter("bw"))
 
 function emit(exp: Expression): string {
     return emitter.get(exp.type)!(exp);
--- compiler/glsl/header.frag
+++ compiler/glsl/header.frag
@@ -2,3 +2,19 @@ precision highp float;
 
 varying vec2 pos;
 uniform float time;
+
+vec3 round(vec3 v) {
+    return floor(0.5 + v);
+}
+
+vec3 ushift(vec3 v) {
+    return (v + 1.0) / 2.0;
+}
+
+vec3 rgb(vec3 r, vec3 g, vec3 b) {
+    return vec3(r.x, g.x, b.x);
+}
+
+vec3 bw(vec3 rgb) {
+    return vec3(0.3 * rgb.x + 0.59 * rgb.y + 0.11 * rgb.z);
+}
