6abaa78143024ae45dbcf65ac01c95ac4eb5ea93
Matthew Hammond <matthewbh95@gmail.com>
2021-06-26 04:57:51 -0500

    Added instructions and README.

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -5,9 +5,24 @@ import Editor from "./Editor";
 export default function App() {
     return (<div>
         <Gallery />
+        <div>
+            You can write your own expression below. Unfortunately it isn't selectable yet.
+        </div>
         {/*<Editor initialSrc={"rgb\n(* (% x t) x)\n(% y t)\n(* x y)"}/>*/}
-        {/*<Editor initialSrc={"rgb % t % x y * t * x y / t / x y"}/>*/}
+        <Editor initialSrc={"rgb % t % x y * t * x y / t / x y"}/>
         {/*<Editor initialSrc={"rgb (* (* t x) y) (* (- 1 t) (- x y)) (* (- 0.5 t) (- 1 x))"}/>*/}
-        <Editor />
+        {/*<Editor />*/}
+        <div>
+            <i>"This art may not make sense to you. It makes Ness sleepy just thinking about it. Use Paralysis to knock some sense into the painting."</i>
+                <br />
+            — EarthBound Player's Guide
+        </div>
+        <br />
+        <div style={{color: "yellow"}}>
+            UX developer wanted :)
+        </div>
+        <div style={{color: "blue"}}>
+            <a href="https://github.com/mbh95/abstract-art">GitHub repo</a>
+        </div>
     </div>);
 }
--- src/view/Gallery.tsx
+++ src/view/Gallery.tsx
@@ -16,7 +16,17 @@ export default function Gallery(props: {}) {
     });
 
     return (<div className="Gallery">
-            <div className="GalleryHeader" style={{display: "flex"}}>
+            <div className="Instructions">
+                <div>
+                    Select expressions you like by clicking on them. Press "Next generation" to breed the selected
+                    expressions together randomly.
+                    <br/>
+                    If you don't like any of the expressions then press "Random" to get new ones.
+                    <br/>
+                </div>
+
+            </div>
+            <div className="Controls" style={{display: "flex"}}>
                 <button onClick={() => dispatch(random())}>Random</button>
                 <button onClick={() => dispatch(nextGeneration())}>Next generation</button>
             </div>
